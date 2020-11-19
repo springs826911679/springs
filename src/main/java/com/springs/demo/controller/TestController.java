53726b481a5632a165af292a8f906e5f4e87f90a
wxwatsmxb <wxw@eduwind.com>
2020-11-19 18:42:18 +0800

    1

--- src/main/java/com/springs/demo/controller/TestController.java
+++ src/main/java/com/springs/demo/controller/TestController.java
@@ -2,6 +2,7 @@ package com.springs.demo.controller;
 
 
 import com.springs.demo.service.GoodsService;
+import com.springs.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @Autowired
     GoodsService goodsService;
+    @Autowired
+    UserService userService;
     @RequestMapping("/index")
     public String test(){
+
+        System.out.println(userService.getCurrentUser());
         return "index";
     }
 }
