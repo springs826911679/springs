8a57bf829303e88086e8f68643c6e89a3f6cec63
[username] <springs@hnuc.edu.cn>
2020-11-19 21:47:56 +0800

    1

--- src/main/java/com/springs/demo/controller/TestController.java
+++ src/main/java/com/springs/demo/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.springs.demo.controller;
 
 
+import com.springs.demo.domain.Goods;
 import com.springs.demo.service.GoodsService;
 import com.springs.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,17 @@ public class TestController {
     UserService userService;
     @RequestMapping("/index")
     public String test(){
-
        // System.out.println(userService.getCurrentUser());
         return "show";
+    }
+    @RequestMapping("/add")
+    public String add(){
+        return "add";
+    }
+    @RequestMapping("addGoods")
+    public String addGoods(@RequestBody Goods goods){
+
+        goodsService.save(goods);
+        return "add";
     }
 }
